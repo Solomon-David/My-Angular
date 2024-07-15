e5f7d00da3003bb9489285881e6b30ebf832f4db
Davidson <davidsoberekon66@gmail.com>
2024-07-14 21:30:49 -0700

    refactor code

---
+++ DavyServer/Routes/Login.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const User = require('../Model/User')
+const LoginRouter = new express.Router()
+
+LoginRouter.post("/login", async function(req,res){
+    try{
+        const {password,username} = req.body
+        
+        const newUser = new User({
+            password,
+            username
+        })
+        await newUser.save()
+        console.log(username)
+        res.send('Login succcessful')
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+module.exports = LoginRouter
